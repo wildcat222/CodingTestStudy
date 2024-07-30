b74f1a586bfe19070aacf5400d2dd2b25a673974
wildcat222 <wildcat222@naver.com>
2024-07-30 17:18:57 +0900

    programmers_level1

---
+++ src/programmers/level1/세균증식.java
@@ -0,0 +1,11 @@
+package programmers.level1;
+
+public class 세균증식 {
+    public int solution(int n, int t) {
+        int answer = n;
+        for (int i = 0; i < t; i++) {
+            answer = answer * 2;
+        }
+        return answer;
+    }
+}
