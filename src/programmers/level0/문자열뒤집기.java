48af6ba7c60e90ea4d9f09a093286573c539f4fe
wildcat222 <wildcat222@naver.com>
2024-08-04 21:13:12 +0900

    programmers_level0

---
+++ src/programmers/level0/문자열뒤집기.java
@@ -0,0 +1,13 @@
+package programmers.level0;
+
+public class 문자열뒤집기 {
+    public String solution(String my_string) {
+        String answer = "";
+
+        for (int i = my_string.length() - 1; i >= 0; i--) {
+            answer += my_string.charAt(i);
+        }
+
+        return answer;
+    }
+}
