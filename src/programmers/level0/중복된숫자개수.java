1fa24b7e55d1b99ffa3a11cc963e6eb3bdb8e39c
wildcat222 <wildcat222@naver.com>
2024-08-04 21:40:57 +0900

    programmers_level0

---
+++ src/programmers/level0/중복된숫자개수.java
@@ -0,0 +1,15 @@
+package programmers.level0;
+
+public class 중복된숫자개수 {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == n) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
