5f488cd22e19467a6dbaef5b6e0d865273422db4
wildcat222 <wildcat222@naver.com>
2024-08-04 19:08:21 +0900

    programmers_level0

---
+++ src/programmers/level0/중앙값구하기.java
@@ -0,0 +1,14 @@
+package programmers.level0;
+
+import java.util.Arrays;
+
+public class 중앙값구하기 {
+    public int solution(int[] array) {
+        int answer = 0;
+
+        Arrays.sort(array);
+        answer = array[array.length / 2];
+
+        return answer;
+    }
+}
