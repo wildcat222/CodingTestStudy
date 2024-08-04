a90b8fd41900cd190efa69164538928db6e76476
wildcat222 <wildcat222@naver.com>
2024-08-04 21:06:43 +0900

    programmers_level0

---
+++ src/programmers/level0/짝수홀수개수.java
@@ -0,0 +1,18 @@
+package programmers.level0;
+
+public class 짝수홀수개수 {
+    public int[] solution(int[] num_list) {
+        int[] answer = {0, 0};
+
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 == 0) {
+                answer[0]++;
+            } else {
+                answer[1]++;
+            }
+        }
+
+        return answer;
+
+    }
+}
