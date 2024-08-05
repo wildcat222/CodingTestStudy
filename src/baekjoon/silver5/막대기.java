d23d5692d4c8fea218bb5e91af07b4593e77e34b
wildcat222 <wildcat222@naver.com>
2024-08-05 19:02:34 +0900

    programmers_level0

---
+++ src/baekjoon/silver5/막대기.java
@@ -0,0 +1,29 @@
+package baekjoon.silver5;
+
+public class 막대기 {
+    public static void main(String[] args) {
+        int stick = 64;
+        int stick_cnt = 1;
+        int total_cm = 0;
+
+        while (total_cm != x) {
+            if (stick > x) {
+                total_cm = stick / 2;
+                stick_cnt++;
+                if (total_cm >= x) {
+                    stick_cnt--;
+                    stick /= 2;
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
