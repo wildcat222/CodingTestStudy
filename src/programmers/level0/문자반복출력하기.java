768fe5dabc5c5d3a85ec112048d9db200d0eccc3
wildcat222 <wildcat222@naver.com>
2024-08-05 13:42:21 +0900

    programmers_level0

---
+++ src/programmers/level0/문자반복출력하기.java
@@ -0,0 +1,15 @@
+package programmers.level0;
+
+public class 문자반복출력하기 {
+    public String solution(String my_string, int n) {
+        String answer = "";
+
+        for (int i = 0; i < my_string.length(); i++) {
+            for (int j = 0; j < n; j++) {
+                answer += my_string.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+}
