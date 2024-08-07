11e17364c25fb19177ad94d3c66cf0dc72a02d90
wildcat222 <wildcat222@naver.com>
2024-08-07 21:49:25 +0900

    baekjoon_bronze1

---
+++ src/baekjoon/bronze1/단어공부1157.java
@@ -0,0 +1,37 @@
+package baekjoon.bronze1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 단어공부1157 {
+    public static void main(String[] args) {
+//        알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳(26)이 무엇인지 알아내는 프로그램을 작성하시오.
+//        단, 대문자와 소문자를 구분하지 않는다. a
+//        첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다.
+//        단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        char[] ch = str.toCharArray();
+        int[] cnt = new int[27];
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] >= 'A' && ch[i] <= 'Z') {
+                cnt[ch[i] - 'A']++;
+            } else {
+                cnt[ch[i] - 'a']++;
+            }
+        }
+
+        for (int i = 0;  i < cnt.length; i++) {
+            if (max < cnt[i]) {
+                max = cnt[i];
+            } else if (max == cnt[i]) {
+                System.out.println('?');
+            }
+        }
+
+
+    }
+}
