089da6b145b2f18db77918ec9d0938c9c105b515
wildcat222 <wildcat222@naver.com>
2024-08-19 17:50:00 +0900

    baekjoon_bronze2

---
+++ src/baekjoon/bronze2/저항1076.java
@@ -0,0 +1,23 @@
+package baekjoon.bronze2;
+
+import java.util.Scanner;
+
+public class 저항1076 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String[][] str = {
+                {"black", "0", "1"},
+                {"brown", "1", "10"},
+                {"red", "2", "100"},
+                {"orange", "3", "1000"},
+                {"yellow", "4", "10000"},
+                {"green", "5", "100000"},
+                {"blue", "6", "1000000"},
+                {"violet", "7", "10000000"},
+                {"gray", "8", "100000000"},
+                {"white", "8", "1000000000"}
+        };
+
+    }
+}
