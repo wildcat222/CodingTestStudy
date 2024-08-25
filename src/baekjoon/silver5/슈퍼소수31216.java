6ecfa00ea20d2c59260dafa4a6053ea5d3b0da3d
wildcat222 <wildcat222@naver.com>
2024-08-25 23:22:30 +0900

    baekjoon_silver5

---
+++ src/baekjoon/silver5/슈퍼소수31216.java
@@ -0,0 +1,23 @@
+package baekjoon.silver5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class 슈퍼소수31216 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] iarr = new int[N]; // 입력 값을 담을 배열
+        int[] primes = int []; //소수들을 담을 배열
+
+        for(int i = 0; i < N; i++) {
+            iarr[i] = Integer.parseInt(br.readLine());
+        }
+
+
+
+    }
+}
