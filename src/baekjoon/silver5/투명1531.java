f8d30d8205247f59ff94d624a6bffc33e6245e53
wildcat222 <wildcat222@naver.com>
2024-09-01 21:56:29 +0900

    baekjoon_silver5

---
+++ src/baekjoon/silver5/투명1531.java
@@ -0,0 +1,39 @@
+package baekjoon.silver5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 투명1531 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[][] picture = new int[101][101];
+        int N = Integer.parseInt(str.split(" ")[0]);
+        int M = Integer.parseInt(str.split(" ")[1]);
+
+        for(int i = 0; i < N; i++) {
+            String test = br.readLine();
+            int x1 = Integer.parseInt(test.split(" ")[0]);
+            int y1 = Integer.parseInt(test.split(" ")[1]);
+            int x2 = Integer.parseInt(test.split(" ")[2]);
+            int y2 = Integer.parseInt(test.split(" ")[3]);
+            for(int j = x1; j <= x2; j++) {
+                for(int k = y1; k <= y2; k++) {
+                    picture[j][k]++;
+                }
+            }
+
+        }
+
+        int answer = 0;
+        for(int i = 1; i <= 100; i++) {
+            for(int j = 1; j <= 100; j++) {
+                if(picture[j][j] > M) answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
